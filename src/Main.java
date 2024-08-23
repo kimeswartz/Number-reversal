363d78317d19cb7023e0dd5c4b91e3bd975143d7
kimeswartz <kim.e.swartz@gmail.com>
2024-08-23 09:14:30 +0200

    added more detailed notes for the function

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
+    // Method to take an integer and return its reverse
     public static int reverse(int num) {
         int reverse = 0;
+
+        /*
+
+        Se it as two boxes, initially the number is stored in 'userInput',
+        we move the number over to the 'reversed' box, number by number, starting from the last digit.gi
+
+        #1 Loop until the number becomes 0
+        #2 Extract the last digit of the number and add it to the 'reversed' number
+        #3 Remove the last digit from the number
+
+         */
 
         while (num != 0) {
             reverse = reverse * 10 + num % 10;
